7e6a1cd957e4b0b969345b85c52fb8d50c432dec
Raúl <rgarridorasillo@gmail.com>
2022-01-17 13:09:28 +0100

    TAE8Act1

---
+++ src/main/mainApp.java
@@ -0,0 +1,20 @@
+package main;
+
+public class mainApp {
+
+	public static void main(String[] args) {
+		//Haz una clase llamada Persona que siga las siguientes condiciones:
+//		• Sus atributos son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura. No 
+//		queremos que se accedan directamente a ellos. Piensa que modificador de acceso es el 
+//		más adecuado, también su tipo. Si quieres añadir algún atributo puedes hacerlo.
+//		• Por defecto, todos los atributos menos el DNI serán valores por defecto según su tipo (0 
+//		números, cadena vacía para String, etc.). Sexo sera hombre por defecto, usa una 
+//		constante para ello.
+//		• Se implantaran varios constructores:
+//		o Un constructor por defecto.
+//		o Un constructor con el nombre, edad y sexo, el resto por defecto.
+//		o Un constructor con todos los atributos como parámetro.
+
+	}
+
+}
